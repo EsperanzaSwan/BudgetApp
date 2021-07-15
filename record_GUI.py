86b2d3d1baddb68e859765b1475ad08a7c4b50e6
theresama <matheresa37@gmail.com>
2021-07-14 20:47:31 -0500

    Updated record.py

--- record_GUI.py
+++ record_GUI.py
@@ -3,17 +3,24 @@ import json
 from budget import category
 
 
+# load the previous created ledger
+file_path = 'Ledger.json'
+try:
+    with open(file_path, 'r') as file_handle:
+        budget_dict = json.load(file_handle)
+except Exception as err:
+    print(err)
+    budget_dict = dict()
 
 
-
-def check_ledger(category):
+def check_ledger(category_entry):
     # if the category already exists
-    if category in budget_file.keys():
-        amount = budget_file[category][0]['total']
+    if category_entry in budget_dict.keys():
+        amount = budget_dict[category_entry][0]['total']
         # retrieve all the previous ledgers
-        item_ledger = budget_file[category][1:]
+        item_ledger = budget_dict[category_entry][1:]
         # create the budget object
-        the_item = category(category, amount)
+        the_item = category(category_entry, amount)
         # if this is the first time creating the ledger or the category doesn't exist
     else:
         amount = input('This category does not exist. Enter amount to create one or enter done to quit:')
@@ -32,9 +39,10 @@ root.title('Budget App')
 root.geometry("600x400")
 category_label = tk.Label(root, text = 'Please enter a category (e.g., food):', font=('calibre',12, 'bold'))
 
-category_button = tk.Button(root, text = "Confirm", fg = "red", command=check_ledger('Health'))
+category_button = tk.Button(root, text = "Confirm", fg = "red", command=check_ledger('food'))
 # category = tk.StringVar()
 category_label.grid()
 category_button.grid(column=1, row=0)
-
-root.mainloop()
+while True:
+    root.update_idletasks()
+    root.update()
